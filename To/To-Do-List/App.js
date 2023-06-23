acf4a15889f78a1915f9a1da4dd9e95d19a350c9
Alexk781 <alexanderkeery@gmail.com>
2023-06-23 12:52:32 -0700

    error

--- To/To-Do-List/App.js
+++ To/To-Do-List/App.js
@@ -1,15 +1,12 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
-/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    // eslint-disable-next-line react/jsx-no-comment-textnodes
+    
     <View style={styles.container}>
-      // eslint-disable-next-line react/jsx-no-comment-textnodes, react/jsx-no-comment-textnodes
       <Text>Open up App.js to start working on your app!</Text>
-      // eslint-disable-next-line react/react-in-jsx-scope
       <StatusBar style="auto" />
     </View>
   );
